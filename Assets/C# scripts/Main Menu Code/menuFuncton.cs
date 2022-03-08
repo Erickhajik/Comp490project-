061b1fcc43727d4cf8f2ac454a9e8030c8827094
Erickhajik <erickhajik65@gmail.com>
2022-03-07 17:56:40 -0800

    Update menuFuncton.cs

--- Assets/C# scripts/Main Menu Code/menuFuncton.cs
+++ Assets/C# scripts/Main Menu Code/menuFuncton.cs
@@ -6,41 +6,14 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class menuFuncton : MonoBehaviour
 {
-    
-    
-    
-     public void GoNameMenu()
-    {
-        SceneManager.LoadScene("NameMenu");
-    }
-
-    public void GoMainMenu()
-    {
-      
-        SceneManager.LoadScene("MainMenu");
-    }
-
-    public void GoNewGame()
-    {
-        SceneManager.LoadScene("Show");
-    }
-
-    public void GoOptions()
-    {
-        SceneManager.LoadScene("Options");
-    }
-
-    public void GoLeaderBoard()
-    {
-        SceneManager.LoadScene("LeaderBoard");
-    }
-    public void GoCredit()
-    {
-        SceneManager.LoadScene("Credit");
-    }
     public void ExitTheGame()
     {
         Application.Quit();
 
+    }
+
+    public void loadLobby()
+    {
+        SceneManager.LoadScene("Show");
     }
 }
