eb5e1aa808e85e49fe644e4892e124431f78cea2
Erickhajik <erickhajik65@gmail.com>
2022-04-26 19:13:08 -0700

    Create StartLevel2Timer.cs

---
+++ Assets/All different Scenes/Asylum/StartLevel2Timer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevel2Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Level2MainTimer.StartTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
