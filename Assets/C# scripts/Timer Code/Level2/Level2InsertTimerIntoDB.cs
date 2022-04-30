016fb562edc0aefbaf83e0fc0221b44e21ef575d
Erickhajik <erickhajik65@gmail.com>
2022-04-30 13:24:02 -0700

    Revert "Update Level2InsertTimerIntoDB.cs"
    This reverts commit 57e4148425d6fed68741b6d4db8b6a17178062ce.

--- Assets/C# scripts/Timer Code/Level2/Level2InsertTimerIntoDB.cs
+++ Assets/C# scripts/Timer Code/Level2/Level2InsertTimerIntoDB.cs
@@ -11,22 +11,22 @@ public class Level2InsertTimerIntoDB : MonoBehaviour
     {
         WWWForm form = new WWWForm();
         form.AddField("userName", MainMenuCode.userNameSave);
-        form.AddField("gamestart", Level2MainTimer.Level2gamestart);
-        form.AddField("puzzle1start", Level2Puzzle1.Level2puzzle1Start);
-        form.AddField("puzzle1end", Level2Puzzle1.Level2puzzle1End);
-        form.AddField("puzzle2start", Level2Puzzle2.Level2puzzle2Start);
-        form.AddField("puzzle2end", Level2Puzzle2.Level2puzzle2End);
-        form.AddField("puzzle3start", Level2Puzzle3.Level2Puzzle3Start);
-        form.AddField("puzzle3end", Level2Puzzle3.Level2Puzzle3End);
-        form.AddField("endtime", Level2MainTimer.Level2gameend);
-        // form.AddField("gamestart", "");
-        // form.AddField("puzzle1start","" );
-        // form.AddField("puzzle1end", "");
-        // form.AddField("puzzle2start", "");
-        // form.AddField("puzzle2end", "");
-        // form.AddField("puzzle3start", "");
-        // form.AddField("puzzle3end", "");
-        // form.AddField("endtime", "");
+        // form.AddField("gamestart", Level2MainTimer.Level2gamestart);
+        // form.AddField("puzzle1start", Level2Puzzle1.Level2puzzle1Start);
+        // form.AddField("puzzle1end", Level2Puzzle1.Level2puzzle1End);
+        // form.AddField("puzzle2start", Level2Puzzle2.Level2puzzle2Start);
+        // form.AddField("puzzle2end", Level2Puzzle2.Level2puzzle2End);
+        // form.AddField("puzzle3start", Level2Puzzle3.Level2Puzzle3Start);
+        // form.AddField("puzzle3end", Level2Puzzle3.Level2Puzzle3End);
+        // form.AddField("endtime", Level2MainTimer.Level2gameend);
+        form.AddField("gamestart", "Level2MainTimer.Level2gamestart");
+        form.AddField("puzzle1start","Level2MainTimer.Level2gamestart" );
+        form.AddField("puzzle1end", "Level2Puzzle1.Level2puzzle1End");
+        form.AddField("puzzle2start", "Level2Puzzle2.Level2puzzle2Start");
+        form.AddField("puzzle2end", "Level2Puzzle2.Level2puzzle2End");
+        form.AddField("puzzle3start", "Level2Puzzle3.Level2Puzzle3Start");
+        form.AddField("puzzle3end", "Level2Puzzle3.Level2Puzzle3End");
+        form.AddField("endtime", "Level2MainTimer.Level2gameend");
         WWW www = new WWW("http://globaltroop.xyz/Level2Timer.php",form);
         yield return www;
     }
