b822462bf757fee119700342e5173be608b47ef4
andrewsingh417 <andrew.singh.417@my.csun.edu>
2022-05-02 20:08:56 -0700

    fixed move zombie

--- Assets/C# scripts/ZombieFollow.cs
+++ Assets/C# scripts/ZombieFollow.cs
@@ -8,6 +8,7 @@ public class ZombieFollow : MonoBehaviour
   public Transform Player;
 
   public static bool willfollow=true;
+  Vector3 home = new Vector3(-27.30532f,-0.006185174f,-30.09311f);
 
   void Start() {
   }
@@ -18,6 +19,10 @@ public class ZombieFollow : MonoBehaviour
     {
     enemy.SetDestination(Player.position);
 
+    }
+    else
+    {
+      enemy.SetDestination(home);
     }
   }
 }
