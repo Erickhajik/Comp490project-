53d6f6c2d35a7b8d9d6d45b1a1a2e27ee61f7de9
andrewsingh417 <andrew.singh.417@my.csun.edu>
2022-05-08 20:50:40 -0700

    saying stop makes zombie leave

--- Assets/C# scripts/ZombieFollow.cs
+++ Assets/C# scripts/ZombieFollow.cs
@@ -18,11 +18,12 @@ public class ZombieFollow : MonoBehaviour
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
 
   public static bool willfollow=false;
+  public float time = 0;
   Vector3 home = new Vector3(-27.30532f,-0.006185174f,-30.09311f);
   
 
   void Start() {
-    actions.Add("stop", Stop);
+    actions.Add("Stop", Stop);
 
             keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
             keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
@@ -30,7 +31,9 @@ public class ZombieFollow : MonoBehaviour
   }
 
   private void Stop(){
-    enemy.SetDestination(home);
+    time=10;
+    
+
 
   }
 
@@ -42,7 +45,14 @@ public class ZombieFollow : MonoBehaviour
 
   void Update(){
 
-    if(willfollow)
+    if (time > 0)
+        {
+            time -= Time.deltaTime;
+            enemy.SetDestination(home);
+        }
+        else
+        {
+          if(willfollow)
     {
     enemy.SetDestination(Player.position);
 
@@ -51,5 +61,9 @@ public class ZombieFollow : MonoBehaviour
     {
       enemy.SetDestination(home);
     }
+        }
+        
+
+    
   }
 }
